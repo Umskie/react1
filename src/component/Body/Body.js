db73d290a3c334d8b0e45f31a290c22b56e7035e
Umskie <umarfarooqkhalid@gmail.com>
2023-09-04 23:38:25 -0500

    intro

---
+++ src/component/Body/Body.js
@@ -0,0 +1,36 @@
+
+
+const Body = ()=> {
+    return (<div>
+        
+<div id="carouselExample" class="carousel slide">
+    <div class="carousel-inner">
+    
+        <div class="carousel-item active">
+        <img src="https://images.pexels.com/photos/2440021/pexels-photo-2440021.jpeg?auto=compress&cs=tinysrgb&w=1600" class="d-block w-100" alt="..."/>
+        </div>
+    
+        <div class="carousel-item">
+        <img src="https://images.pexels.com/photos/2325446/pexels-photo-2325446.jpeg?auto=compress&cs=tinysrgb&w=1600" class="d-block w-100" alt="..."/>
+        </div>
+    
+        <div class="carousel-item">
+        <img src="https://images.pexels.com/photos/1155577/pexels-photo-1155577.jpeg?auto=compress&cs=tinysrgb&w=1600" class="d-block w-100" alt="..."/>
+        </div>
+    </div>
+
+
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+
+</div>
+
+            </div>)
+}
+export default Body
