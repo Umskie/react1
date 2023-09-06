0293ac6c0f76f43b1adca71adcb630043ef57c33
Umskie <umarfarooqkhalid@gmail.com>
2023-09-05 20:48:14 -0500

    Added my project

--- src/component/Body/Body.js
+++ src/component/Body/Body.js
@@ -31,6 +31,42 @@ const Body = ()=> {
 
 </div>
 
+{/* Cards */}
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+    <div class="card" >
+  <img class="card-img-top" src="https://images.pexels.com/photos/7605625/pexels-photo-7605625.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="Card image cap"/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    </div>
+    <div class="col-sm">
+    <div class="card" >
+  <img class="card-img-top" src="https://images.pexels.com/photos/18168998/pexels-photo-18168998/free-photo-of-close-up-of-orange-flowers.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="Card image cap"/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    </div>
+    <div class="col-sm">
+    <div class="card" >
+  <img class="card-img-top" src="https://images.pexels.com/photos/9030684/pexels-photo-9030684.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="Card image cap"/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+
             </div>)
 }
 export default Body
